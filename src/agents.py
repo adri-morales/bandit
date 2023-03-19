049f1ddffaab43b1ceebc8371667784e6a5d6dbf
Amoralpa11 <drnmoralespastor@gmail.com>
2023-03-19 12:17:37 +0100

    Implemented epsilon greedy
    Also changed the name of the module

--- src/agents.py
+++ src/agents.py
@@ -17,6 +17,7 @@ class BanditAgent:
             n_actions (int): The number of available actions.
             lr (float): The learning rate used to update the policy.
         """
+        self.n_actions = n_actions
         self.policy = np.random.normal(0, 1, n_actions)
         self.lr = lr
 
@@ -44,3 +45,37 @@ class BanditAgent:
             next_obs (int): The observation obtained after taking the action.
         """
         self.policy[action] += self.lr * (reward - self.policy[action])
+        
+class EGreedyBanditAgent(BanditAgent):
+    
+    def __init__(self, n_actions: int, lr: float, epsilon: float):
+        """
+        Initializes the e-greedy bandit agent with a policy, learning rate, and exploration rate.
+
+        Args:
+            n_actions (int): The number of actions the agent can take.
+            lr (float): The learning rate used to update the policy.
+            epsilon (float): The exploration rate used to select a random action.
+        """
+        super().__init__(n_actions, lr)
+        self.epsilon = epsilon
+        
+    def get_action(self, obs: int) -> int:
+        """
+        Selects an action given the current policy. Selects optimal action with probability 1-epsilon
+        and a random action with probability epsilon
+
+        Args:
+            obs (int): The current observation.
+
+        Returns:
+            int: The index of the selected action.
+        """
+        
+        rand = np.random.random()
+        if rand < self.epsilon:
+            action = np.random.randint(0, self.n_actions)
+        else:
+            action = np.argmax(self.policy)
+            
+        return action
