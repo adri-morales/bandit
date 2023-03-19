f9d9c0ff3a0f9179c6d1d1a7342f71da39008046
Amoralpa11 <drnmoralespastor@gmail.com>
2023-03-19 09:13:03 +0100

    change the type of observation in methods

--- src/agent.py
+++ src/agent.py
@@ -20,27 +20,27 @@ class BanditAgent:
         self.policy = np.random.normal(0, 1, n_actions)
         self.lr = lr
 
-    def get_action(self, obs: np.ndarray) -> int:
+    def get_action(self, obs: int) -> int:
         """
         Selects the optimal action given the current policy.
 
         Args:
-            obs (np.ndarray): The current observation.
+            obs (int): The current observation.
 
         Returns:
             int: The index of the optimal action.
         """
         return np.argmax(self.policy)
 
-    def update(self, obs: np.ndarray, action: int, reward: float, terminated: bool, next_obs: np.ndarray) -> None:
+    def update(self, obs: int, action: int, reward: float, terminated: bool, next_obs: int) -> None:
         """
         Updates the policy given the current transition.
 
         Args:
-            obs (np.ndarray): The current observation.
+            obs (int): The current observation.
             action (int): The action taken in the current observation.
             reward (float): The reward received for taking the action.
             terminated (bool): Whether or not the episode has terminated.
-            next_obs (np.ndarray): The observation obtained after taking the action.
+            next_obs (int): The observation obtained after taking the action.
         """
         self.policy[action] += self.lr * (reward - self.policy[action])
