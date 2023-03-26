fb5c46b7a7013cb6a5f02a5cedc5f88596955acc
Amoralpa11 <drnmoralespastor@gmail.com>
2023-03-26 05:45:32 +0200

    make trian work with config files

--- src/train.py
+++ src/train.py
@@ -4,8 +4,12 @@ import pandas as pd
 from tqdm import tqdm
 import argparse
 import ast
+import yaml
+import os
 
-sys.path.append('src/agents')
+for subdir, _, _ in os.walk('src/agents'):
+    sys.path.append(subdir)
+
 import greedy
 import egreedy
 import optimistic
@@ -13,24 +17,12 @@ import ucb
 
 parser = argparse.ArgumentParser(description='Run a reinforcement learning agent.')
 # Agent options
-parser.add_argument('-a', '--agent', type=str, choices=['greedy', 'epsilon_greedy', 'ucb', 'optimistic_greedy'], required=True,
-                    help='the agent to use for training')
-
-# Agent parameter options
-parser.add_argument('-p', '--parameters', nargs='+', required=True,
-                    help='the parameters for the agent in the format <parameter_name>:<value>')
+parser.add_argument('-c', '--config', type=str, default='config.yaml')
 
 args = parser.parse_args()
 
-# Parse agent parameters into a dictionary
-agent_params = {}
-if args.parameters is not None:
-    for param in args.parameters:
-        key, value = param.split(':')
-        
-        value = ast.literal_eval(value)
-            
-        agent_params[key] = value
+with open(args.config, 'r') as f:
+    config = yaml.safe_load(f)
 
 n_episodes = 1000
 n_actions = 10
@@ -41,7 +33,11 @@ agent_dict = {'greedy': greedy.BanditAgent,
               'ucb': ucb.UCBBanditAgent,
               'optimistic_greedy': optimistic.OptimisticGreedyBanditAgent}
 
-agent_class = agent_dict[args.agent]
+agent_str = config['agent']
+agent_class = agent_dict[agent_str]
+
+del config['agent']
+agent_params = config
 
 env = MultiArmedBandit(n_actions)
 
@@ -61,7 +57,7 @@ for episode in tqdm(range(n_episodes)):
         
         obs = new_obs
         
-        history.append([episode, step, reward, action, args.agent])
+        history.append([episode, step, reward, action, agent_str])
         
 history = pd.DataFrame(history, columns=['episode', 'step', 'reward', 'action', 'agent'])
-history.to_csv(f'data/results/{args.agent}_history.csv')
+history.to_csv(f'results/{agent_str}_history.csv')
